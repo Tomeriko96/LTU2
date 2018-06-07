69813902e9681ea5bb97cde3882772fcfb58a352
Tomer Iwan <IwanTomer@hotmail.com>
2018-06-07 11:14:05 +0200

    Loading screen (commented)
    Loading screen made, cannot yet set it to remove itself after page loads

---
+++ _site/assets/main.js
@@ -0,0 +1,9 @@
+// function myFunction() {
+//     document.getElementById("demo").style.fontSize = "25px";
+//     document.getElementById("demo").style.color = "red";
+//     document.getElementById("demo").style.backgroundColor = "yellow";
+// }
+// window.addEventListener("load", function (){
+//   var load_screen = document.getElementById("load_screen");
+//   document.body.removeChild(load_screen);
+// })
