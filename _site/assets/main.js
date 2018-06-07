4f0e1ffa429d3d8a6be8b4f7581b3ddae1ae3391
atilla50 <atilla50@users.noreply.github.com>
2018-06-07 17:18:10 +0200

    Test

--- _site/assets/main.js
+++ _site/assets/main.js
@@ -1,4 +1,4 @@
-// function myFunction() {
+/// function myFunction() {
 //     document.getElementById("demo").style.fontSize = "25px";
 //     document.getElementById("demo").style.color = "red";
 //     document.getElementById("demo").style.backgroundColor = "yellow";
