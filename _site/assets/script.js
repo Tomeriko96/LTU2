e071e51dbdbf634755a8034ebc6d7749c424a6a1
atilla50 <atilla50@users.noreply.github.com>
2018-06-08 18:17:57 +0200

    Loading Page integrated to index.html
    Loading page now works without moving to a new window.
    Index2.html stuff moved to Index.html.
    Index2.html -> useless. Style.css -> useless.

--- _site/assets/script.js
+++ _site/assets/script.js
@@ -5,12 +5,12 @@
 	var id = setInterval(frame, 64);
 	
 	function frame() {
-		if(loading == 100) {
+		if(loading == 70) {
 			clearInterval(id);
-			window.open("index2.html", "_self");
+			$('#preload').remove();
 		} else {
 			loading = loading + 1;
-			if(loading == 90) {
+			if(loading == 60) {
 				preload.style.animation = "fadeout 1s ease";
 			}
 		}
