18c810d29efaf7e674bcb5c3ccf338289567b06e
atilla50 <atilla50@users.noreply.github.com>
2018-06-10 16:14:19 +0200

    Button to scroll up added

--- _site/assets/main.js
+++ _site/assets/main.js
@@ -51,3 +51,29 @@ function hasScrolled() {
     
     lastScrollTop = st;
 }
+
+$(document).ready(function(){
+
+	// hide #back-top first
+	$("#up").hide();
+	
+	// fade in #back-top
+	$(function () {
+		$(window).scroll(function () {
+			if ($(this).scrollTop() > 100) {
+				$('#up').fadeIn();
+			} else {
+				$('#up').fadeOut();
+			}
+		});
+
+		// scroll body to 0px on click
+		$('#up').click(function () {
+			$('body,html').animate({
+				scrollTop: 0
+			}, 500);
+			return false;
+		});
+	});
+
+});
